6f46cadfb85d38a57544e09b6c066d50801cf541
Bodapati Jaswanth Venkata Ratna Rao <122739729+BodapatiJaswanthVenkataRatnaRao@users.noreply.github.com>
2025-04-25 17:48:44 +0530

    Time: 2 ms (97.42%), Space: 41.6 MB (82.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='['||ch=='{'){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                char c=stack.pop();
+                if(ch==')' && c!='(' || ch=='}' && c!='{' || ch==']' && c!='[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
